c6972e304698567491152861de872c1ccebbf1c0
Linqing Wang <wlqpku@gmail.com>
2013-12-15 21:24:30 +0800

    testcode

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {  
+  public static void main(String[] args) {  
+    int i = 10000;
+	i=i+1;
+	int j=0;
+	j = j+i;  
+    System.out.println("Hello Bytecode! Numberi = " + i);  
+    System.out.println("Hello Bytecode! Numberj = " + j);  
+  }  
+} 
